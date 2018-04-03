7f82dce0089c16655f39cff1fea76508be51541e
suwigyarathore <Suwi.Rathore@klm.com>
2018-04-03 06:41:55 +0200

    Adding factory design pattern

---
+++ Creational Pattern/factory/index.js
@@ -0,0 +1,7 @@
+var userFactory = require('./userFactory');
+
+var alex = userFactory('Alex Banks', 100);
+var eve = userFactory('Eve Porcello', 100, 'employee', 'This and That');
+
+console.log(alex.toString())
+console.log(eve.toString())
